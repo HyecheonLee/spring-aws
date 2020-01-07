17cbea25cad7738ceb1c8f5d921573da771adb8c
hyecheon <rainbow880616@gmail.com>
2020-01-08 01:23:06 +0900

    react-router 적용

---
+++ frontend/src/components/PostSave.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostSave = () => {
+  return (
+      <div>
+        <h1>게시글 등록</h1>
+      </div>
+  );
+};
+
+export default PostSave;
---
+++ frontend/src/containers/PostSaveContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostSave from "../components/PostSave";
+
+const PostSaveContainer = () => {
+  return (
+      <div>
+        <PostSave/>
+      </div>
+  );
+};
+
+export default PostSaveContainer;
