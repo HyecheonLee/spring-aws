5ab9b92bb283958cf5f74e438623421846a60ae1
hyecheon <rainbow880616@gmail.com>
2020-01-10 00:41:18 +0900

    list - all 추가.

--- src/main/java/com/hyecheon/service/post/PostService.java
+++ src/main/java/com/hyecheon/service/post/PostService.java
@@ -5,32 +5,40 @@ import com.hyecheon.domain.post.Post;
 import com.hyecheon.domain.post.PostRepository;
 import com.hyecheon.web.dto.PostResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Transactional
 @RequiredArgsConstructor
 @Service
 public class PostService {
-    private final PostRepository postRepository;
 
-    public PostResponseDto save(Post post) {
-        return new PostResponseDto(postRepository.save(post));
-    }
+  private final PostRepository postRepository;
 
-    public PostResponseDto save(Long id, Post post) {
-        final var foundPost = postRepository.findById(id).orElseThrow(() -> new NotFoundException("[ id : " + id + "] 를 찾을수 없습니다."));
-        foundPost.update(post);
-        return new PostResponseDto(foundPost);
-    }
+  public PostResponseDto save(Post post) {
+    return new PostResponseDto(postRepository.save(post));
+  }
 
-    public PostResponseDto getById(Long id) {
-        return new PostResponseDto(postRepository.findById(id).orElseThrow(() -> new NotFoundException("[ id : " + id + "] 를 찾을수 없습니다.")));
-    }
+  public Page<PostResponseDto> findAll(Pageable pageable) {
+    final var posts = postRepository.findAll(pageable);
+    return posts.map(PostResponseDto::new);
+  }
 
-    public void deleteById(Long id) {
-        postRepository.deleteById(id);
-    }
+  public PostResponseDto save(Long id, Post post) {
+    final var foundPost = postRepository.findById(id)
+        .orElseThrow(() -> new NotFoundException("[ id : " + id + "] 를 찾을수 없습니다."));
+    foundPost.update(post);
+    return new PostResponseDto(foundPost);
+  }
+
+  public PostResponseDto getById(Long id) {
+    return new PostResponseDto(postRepository.findById(id)
+        .orElseThrow(() -> new NotFoundException("[ id : " + id + "] 를 찾을수 없습니다.")));
+  }
+
+  public void deleteById(Long id) {
+    postRepository.deleteById(id);
+  }
 }
--- src/main/java/com/hyecheon/web/PostApiController.java
+++ src/main/java/com/hyecheon/web/PostApiController.java
@@ -1,44 +1,58 @@
 package com.hyecheon.web;
 
-import com.hyecheon.domain.post.Post;
 import com.hyecheon.service.post.PostService;
 import com.hyecheon.web.dto.PostResponseDto;
 import com.hyecheon.web.dto.PostSaveRequestDto;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/posts")
 @RestController
 public class PostApiController {
-    private final PostService postService;
 
+  private final PostService postService;
 
-    @PostMapping
-    public ResponseEntity<?> save(@RequestBody PostSaveRequestDto postSaveRequestDto) {
-        final PostResponseDto savePost = postService.save(postSaveRequestDto.toEntity());
-        return ResponseEntity.created(URI.create("/api/v1/posts/" + savePost.getId())).body(savePost);
-    }
+  @GetMapping
+  public ResponseEntity<?> readAll(Pageable pageable) {
+    final var posts = postService.findAll(pageable);
+    return ResponseEntity.ok(posts);
+  }
 
-    @PutMapping("/{id:[0-9]+}")
-    public ResponseEntity<?> save(@PathVariable Long id, @RequestBody PostSaveRequestDto postSaveRequestDto) {
-        final PostResponseDto savePost = postService.save(id, postSaveRequestDto.toEntity());
-        return ResponseEntity.ok().body(savePost);
-    }
+  @PostMapping
+  public ResponseEntity<?> save(@RequestBody PostSaveRequestDto postSaveRequestDto) {
+    final PostResponseDto savePost = postService.save(postSaveRequestDto.toEntity());
+    return ResponseEntity.created(URI.create("/api/v1/posts/" + savePost.getId())).body(savePost);
+  }
 
-    @GetMapping("/{id:[0-9]+}")
-    public ResponseEntity<?> read(@PathVariable Long id) {
-        final PostResponseDto findPost = postService.getById(id);
-        return ResponseEntity.ok().body(findPost);
-    }
+  @PutMapping("/{id:[0-9]+}")
+  public ResponseEntity<?> save(@PathVariable Long id,
+      @RequestBody PostSaveRequestDto postSaveRequestDto) {
+    final PostResponseDto savePost = postService.save(id, postSaveRequestDto.toEntity());
+    return ResponseEntity.ok().body(savePost);
+  }
 
-    @DeleteMapping("/{id:[0-9]+}")
-    public ResponseEntity<?> delete(@PathVariable Long id) {
-        postService.deleteById(id);
-        return ResponseEntity.ok("");
-    }
+  @GetMapping("/{id:[0-9]+}")
+  public ResponseEntity<?> read(@PathVariable Long id) {
+    final PostResponseDto findPost = postService.getById(id);
+    return ResponseEntity.ok().body(findPost);
+  }
+
+  @DeleteMapping("/{id:[0-9]+}")
+  public ResponseEntity<?> delete(@PathVariable Long id) {
+    postService.deleteById(id);
+    return ResponseEntity.ok("");
+  }
 }
